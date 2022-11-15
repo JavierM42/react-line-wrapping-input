3e79500b7a9198cd255b191850c8affa194fd016
JavierM42 <javier@wyeworks.com>
2022-11-15 13:59:59 -0300

    Add and document more feaures

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # react-line-wrapping-input
 
-`<input type="text" />` elements don't adjust to the content.
+`<input type="text" />` elements don't wrap.
 
-`<textarea />` elements don't either, and they allow line breaks.
+`<textarea />` elements don't adjust to the content, and they allow line breaks.
+
+Sometimes, you need an input that wraps when the content is too long.
 
 Sometimes, you need an input that wraps when the content is too long, but doesn't allow manual line breaks.
 
@@ -30,16 +32,58 @@ import LineWrappingInput from "react-line-wrapping-input";
 
 ## How it works
 
-It renders a `div` element with `display: grid` and a `textarea` child, as well as an invisible `div` on the same grid area. The invisible div renders the textarea's content, and since divs automatically resize according to their content, the textarea does as well.
+It renders a `div` element with `display: grid` and a `textarea` child, as well as another `div` that overlaps it. This `div` renders the textarea's content too (but invisible!), and since divs automatically resize according to their content, the textarea does as well.
 
-This idea comes from [this CSS tricks article](https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/), with the added feature of emulating a single-line input.
+This idea comes from [this CSS tricks article](https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/), with the added feature of emulating a single-line input by stripping line breaks.
 
-## Extra features
+## Styling
 
-There are a few extra features included.
+You can add CSS rules to the built-in classes, supply your own, or use inline styles.
 
-### Suffix
+### Built-in classes:
+
+- Use the `.line-wrapping-input-container` selector to style the container.
+- Use the `.line-wrapping-input` selecctor to style the textarea and the invisible element.
+
+### Supplying your own classes:
+
+- Use the `containerClassName` prop to add classes to the container.
+- Use the `className` prop to add classes to the textarea and the invisible element.
+
+### Inline styles:
+
+- Use the `containerStyle` prop to add styles to the container. _`display: grid` cannot be overridden_
+- Use the `style` prop to add styles to the textarea and the invisible element. _`grid-area`, `resize`, `white-space` and `overflow-wrap` cannot be overridden._
+
+> A way to style only the textarea or the invisible element is not provided because if they don't have matching styles the autosizing would break.
+
+## Extras
+
+There are a few extra features that you can take advantage of on the line-wrapping input.
+
+### blurOnLineBreak
+
+The `blurOnLineBreak` boolean prop will blur the input when the user presses the `return` key. Otherwise, the return key will move the cursor to the end of the content.
+
+> On regular HTML text inputs, the return key doesn't behave like this, but I've found no way to prevent it.
+
+### onReturn
+
+If you want to do anything else when the user presses the return key (for example, persisting the data), use the `onReturn` prop.
+
+### suffix
 
 The `suffix` optional prop renders some static text after the editable portion of the input. It can be useful for fields with units, such as "X minutes".
 
-The suffix is rendered in a `span` tag. Use the `suffixStyle` and/or `suffixClassName` props to style it.
+The suffix is rendered in a `span` tag. Use the `suffixStyle` and/or `suffixClassName` props to style it if you need to.
+
+### ref
+
+The `ref` to the textarea element is exposed via forwardRef. Use it only if you know what you're doing.
+
+## Limitations and workarounds
+
+### Maximum rows
+
+There's no way to set the maximum amount of rows for the input. If you need the input not to expand past a certain point, set `max-height` (and most likely `overflow: auto`) on the container element (with the `.line-wrapping-input-container` selector, the `containerClassName` prop or the `containerStyle` prop).
+
--- src/LineWrappingInput.tsx
+++ src/LineWrappingInput.tsx
@@ -1,7 +1,7 @@
 import React, {
   ChangeEventHandler,
   CSSProperties,
-  FC,
+  forwardRef,
   HTMLProps,
   useCallback,
   useEffect,
@@ -9,86 +9,116 @@ import React, {
 } from "react";
 
 type Props = Omit<HTMLProps<HTMLTextAreaElement>, "aria-multiline" | "rows"> & {
+  containerClassName?: string;
+  containerStyle?: Omit<CSSProperties, "display">;
+  blurOnLineBreak?: boolean;
+  onReturn?: ChangeEventHandler<HTMLTextAreaElement>;
   suffix?: string;
   suffixClassName?: string;
   suffixStyle?: CSSProperties;
 };
 
-const LineWrappingInput: FC<Props> = ({
-  suffix = " ",
-  suffixClassName,
-  suffixStyle,
-  ...props
-}) => {
-  const [value, setValue] = useState(props.value);
-  useEffect(() => setValue(props.value), [props.value]);
-
-  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = useCallback(
-    (event) => {
-      // Override user typing line breaks. Doesn't fire if the line break was pasted, that's handled by the else block.
-      if ((event.nativeEvent as InputEvent).inputType === "insertLineBreak") {
-        //   event.target.blur();
-      } else {
-        // Strip line breaks (from pasted text or any other reason)
-        const newValue = event.target.value.replace(/\r|\n/g, "");
-        props.onChange?.({
-          ...event,
-          target: {
-            ...event.target,
-            value: newValue,
-          } as EventTarget,
-        });
-        setValue(newValue);
-      }
+const LineWrappingInput = forwardRef<HTMLTextAreaElement, Props>(
+  (
+    {
+      containerClassName,
+      containerStyle = {},
+      suffix = " ",
+      suffixClassName,
+      suffixStyle,
+      blurOnLineBreak,
+      onReturn,
+      ...props
     },
-    [props.onChange, setValue]
-  );
+    ref
+  ) => {
+    const [value, setValue] = useState(props.value);
+    useEffect(() => setValue(props.value), [props.value]);
 
-  return (
-    <div style={{ display: "grid" }}>
-      <textarea
-        {...props}
-        value={value}
-        aria-multiline="false"
-        style={{
-          border: "1px solid black",
-          padding: "2px",
-          font: "inherit",
-          ...props.style,
-          gridArea: "1 / 1 / 2 / 2",
-          resize: "none",
-          overflow: "clip",
-          overflowWrap: "break-word",
-        }}
-        onChange={handleChange}
-        rows={1}
-      />
+    const handleChange: ChangeEventHandler<HTMLTextAreaElement> = useCallback(
+      (event) => {
+        const newValue = event.target.value.replace(/\r|\n/g, "");
+        // Override user typing line breaks. Doesn't fire if the line break was pasted, that's handled by the else block.
+        if ((event.nativeEvent as InputEvent).inputType === "insertLineBreak") {
+          if (blurOnLineBreak) {
+            event.target.blur();
+          }
+          onReturn?.({
+            ...event,
+            target: {
+              ...event.target,
+              value: newValue,
+            } as any,
+          });
+        } else {
+          // Strip line breaks (from pasted text or any other reason)
+          props.onChange?.({
+            ...event,
+            target: {
+              ...event.target,
+              value: newValue,
+            } as any,
+          });
+          setValue(newValue);
+        }
+      },
+      [props.onChange, setValue]
+    );
+
+    return (
       <div
-        style={{
-          gridArea: "1 / 1 / 2 / 2",
-          // textarea style
-          border: "1px solid black",
-          padding: "2px",
-          font: "inherit",
-          whiteSpace: "pre-wrap",
-          overflowWrap: "break-word",
-          pointerEvents: "none",
-        }}
+        className={`line-wrapping-input-container ${containerClassName || ""}`}
+        style={{ ...containerStyle, display: "grid" }}
       >
-        <span
-          aria-hidden="true"
+        <textarea
+          {...props}
+          className={`line-wrapping-input ${props.className || ""}`}
+          value={value}
+          aria-multiline="false"
           style={{
-            visibility: "hidden",
+            border: "1px solid black",
+            padding: "2px",
+            font: "inherit",
+            ...(props.style || {}),
+            gridArea: "1 / 1 / 2 / 2",
+            resize: "none",
+            overflow: "clip",
+            whiteSpace: "pre-wrap",
+            overflowWrap: "break-word",
+          }}
+          onChange={handleChange}
+          rows={1}
+          ref={ref}
+        />
+        <div
+          className={`line-wrapping-input ${props.className || ""}`}
+          style={{
+            border: "1px solid black",
+            padding: "2px",
+            font: "inherit",
+            ...(props.style || {}),
+            gridArea: "1 / 1 / 2 / 2",
+            whiteSpace: "pre-wrap",
+            overflow: "clip",
+            overflowWrap: "break-word",
+            pointerEvents: "none",
           }}
         >
-          {value || props.placeholder}
-        </span>
-        <span className={suffixClassName} style={suffixStyle}>
-          {suffix}
-        </span>
+          <span
+            aria-hidden="true"
+            style={{
+              visibility: "hidden",
+            }}
+          >
+            {value || props.placeholder}
+          </span>
+          <span className={suffixClassName} style={suffixStyle}>
+            {suffix}
+          </span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default LineWrappingInput;

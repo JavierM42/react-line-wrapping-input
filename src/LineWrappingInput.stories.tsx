51aa8a99adae60d6cd6f0419626bb035d1d7c246
JavierM42 <javier@wyeworks.com>
2022-11-15 15:06:11 -0300

    update stories

--- src/LineWrappingInput.stories.tsx
+++ src/LineWrappingInput.stories.tsx
@@ -1,5 +1,14 @@
 import LineWrappingInput from "./LineWrappingInput";
 
 export const Story = () => (
-  <LineWrappingInput placeholder="Some amount of" suffix=" minutes" />
+  <LineWrappingInput
+    placeholder="Some amount of"
+    suffix=" minutes"
+    containerStyle={{
+      border: "1px solid black",
+    }}
+    style={{
+      padding: "2px",
+    }}
+  />
 );
